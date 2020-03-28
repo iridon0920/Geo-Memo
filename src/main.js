0eda5f04e6a46929bc92c41b71c35d2d34bd6da8
iridon0920 <iridon0920@gmail.com>
2020-03-28 06:17:12 +0000

    追加:Google Map連携、画像の差し替えなど

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAP_KEY,
+    libraries: 'places',
+    region: 'JP',
+    language: 'ja'
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
