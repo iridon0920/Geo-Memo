c949afa078e7ee1f444a25ea0153066645dfece5
iridon0920 <iridon0920@gmail.com>
2020-03-31 14:59:08 +0000

    追加:Element UI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(VueGoogleMaps, {
   load: {
@@ -15,6 +17,7 @@ Vue.use(VueGoogleMaps, {
 )
 
 Vue.use(VueAxios, axios)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
