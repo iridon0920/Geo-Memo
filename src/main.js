c1b544f693ce3b20212e5075bd890fe78835b320
iridon0920 <iridon0920@gmail.com>
2020-03-31 14:21:43 +0000

    追加:axiosプラグイン

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(VueGoogleMaps, {
   load: {
@@ -9,7 +11,10 @@ Vue.use(VueGoogleMaps, {
     region: 'JP',
     language: 'ja'
   }
-})
+}
+)
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
